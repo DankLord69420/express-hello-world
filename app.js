71128ef9bdbbf440ae5747913f6200c3050d050f
DankLord69420 <stevecraftkid@gmail.com>
2023-12-07 21:09:22 +0000

    Hosts Corretos

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-const port = 3001; 
+const port = 3001;
 
 const connection = mysql.createConnection({
-    host: '195.35.53.19',
-    user: 'u290118015_DB_Insanity',
-    password: 'u290118015_Insanity69',
-    database: 'u.BnV@BNb95X@hP',
+  host: '195.35.53.19',
+  user: 'u290118015_Insanity69',
+  password: 'u.BnV@BNb95X@hP',
+  database: 'u290118015_DB_Insanity',
 });
 
 connection.connect();
 
 app.get('/api/fetch/:table/', (req, res) => {
-    const { table } = req.params;
+  const { table } = req.params;
 
-    const query = `SELECT * FROM ${table} ORDER BY resultado DESC LIMIT 10`;
-    connection.query(query, (error, results) => {
-        if (error) throw error;
-        res.json(results);
-    });
+  const query = `SELECT * FROM ${table} ORDER BY resultado DESC LIMIT 10`;
+  connection.query(query, (error, results) => {
+    if (error) throw error;
+    res.json(results);
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
