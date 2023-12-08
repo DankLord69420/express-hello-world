205623bd12885b9742e231a47a2d0905947b1d40
DankLord69420 <stevecraftkid@gmail.com>
2023-12-08 17:59:41 +0000

    fix v2

--- controllers/touroController.js
+++ controllers/touroController.js
@@ -1,18 +1,8 @@
 const Touro = require('../models/touro');
 const sequelize = require('../models/database');
 
-const controllers = {};
-
-// Ensure the database connection is established before syncing models
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Database connection has been established successfully.');
-    await sequelize.sync(); // Sync models after the connection is established
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-})();
+const controllers = {}
+sequelize.sync()
 
 controllers.list = async (req, res) => {
   try {
@@ -24,9 +14,7 @@ controllers.list = async (req, res) => {
   } catch (err) {
     res.status(500).json({
       success: false,
-      error: 'Error: ' + err.message,
+      error: 'Erro: ' + err.message,
     });
   }
 };
-
-module.exports = controllers;
