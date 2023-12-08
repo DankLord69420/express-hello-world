6a87d72fa12e1e3a250d18ac967ee3f7d4a26f13
DankLord69420 <stevecraftkid@gmail.com>
2023-12-08 17:46:05 +0000

    Esperar pela DB

--- controllers/touroController.js
+++ controllers/touroController.js
@@ -1,20 +1,32 @@
 const Touro = require('../models/touro');
 const sequelize = require('../models/database');
 
-const controllers = {}
-sequelize.sync()
+const controllers = {};
+
+// Ensure the database connection is established before syncing models
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
+    await sequelize.sync(); // Sync models after the connection is established
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
 
 controllers.list = async (req, res) => {
-    try {
-      const data = await Touro.findAll();
-      res.json({
-        success: true,
-        data: data,
-      });
-    } catch (err) {
-      res.status(500).json({
-        success: false,
-        error: 'Erro: ' + err.message,
-      });
-    }
-  };
+  try {
+    const data = await Touro.findAll();
+    res.json({
+      success: true,
+      data: data,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: 'Error: ' + err.message,
+    });
+  }
+};
+
+module.exports = controllers;
