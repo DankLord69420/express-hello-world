5ca54214293466a3b4377bf9372f0b8c0ab9bdc4
DankLord69420 <stevecraftkid@gmail.com>
2023-12-08 17:42:46 +0000

    Logging

--- models/database.js
+++ models/database.js
@@ -1,16 +1,23 @@
 const Sequelize = require("sequelize");
+
 const sequelize = new Sequelize(
- 'u290118015_DB_Insanity',
- 'u290118015_Insanity69',
- 'u.BnV@BNb95X@hP',
-  {
-    host: '195.35.53.19',
-    dialect: 'mysql'
-  }
+   'u290118015_DB_Insanity',
+   'u290118015_Insanity69',
+   'u.BnV@BNb95X@hP',
+   {
+      host: '195.35.53.19',
+      dialect: 'mysql',
+      logging: console.log, // Log SQL queries to the console for debugging
+   }
 );
 
-sequelize.authenticate().then(() => {
-    console.log('Connection has been established successfully.');
- }).catch((error) => {
-    console.error('Unable to connect to the database: ', error);
- });
+sequelize
+   .authenticate()
+   .then(() => {
+      console.log('Connection has been established successfully.');
+   })
+   .catch(err => {
+      console.error('Unable to connect to the database:', err);
+   });
+
+module.exports = sequelize;
