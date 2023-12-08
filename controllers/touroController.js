7266128862196a695425fb239aaf27d21aa85d26
DankLord69420 <stevecraftkid@gmail.com>
2023-12-08 18:01:58 +0000

    bamos ber

--- controllers/touroController.js
+++ controllers/touroController.js
@@ -1,8 +1,18 @@
 const Touro = require('../models/touro');
 const sequelize = require('../models/database');
 
-const controllers = {}
-sequelize.sync()
+const controllers = {};
+
+// Ensure the database connection is established before syncing models
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
+    await sequelize.sync(); // Sync models after the connection is established
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
 
 controllers.list = async (req, res) => {
   try {
@@ -14,7 +24,9 @@ controllers.list = async (req, res) => {
   } catch (err) {
     res.status(500).json({
       success: false,
-      error: 'Erro: ' + err.message,
+      error: 'Error: ' + err.message,
     });
   }
 };
+
+module.exports = controllers;
