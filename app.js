a423aee4abf4468986121d70a9b1a67fb076390a
DankLord69420 <stevecraftkid@gmail.com>
2023-12-08 18:41:42 +0000

    Connection Polling

--- app.js
+++ app.js
@@ -14,18 +14,30 @@ const connection = mysql.createConnection({
   user: 'u290118015_Insanity69',
   password: 'u.BnV@BNb95X@hP',
   database: 'u290118015_DB_Insanity',
+  connectionLimit: 10,
+  connectTimeout: 30000,
+  acquireTimeout: 30000,
 });
 
-connection.connect();
+try {
+  connection.connect();
+} catch (error) {
+  console.error("Error connecting to MySQL:", error.message);
+}
 
 app.get('/api/fetch/:table/', (req, res) => {
   const { table } = req.params;
 
   const query = `SELECT * FROM ${table} ORDER BY resultado DESC LIMIT 10`;
   connection.query(query, (error, results) => {
-    if (error) throw error;
-    res.json(results);
+    if (error) {
+      console.error("Error executing query:", error.message);
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      res.json(results);
+    }
   });
+
 });
 
 app.listen(port, () => {
