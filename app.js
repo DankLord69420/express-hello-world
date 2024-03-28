aefced211cc79008060800b511197a264ce167b2
DankLord69420 <stevecraftkid@gmail.com>
2024-03-28 18:05:43 +0000

    allow todos os headers

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const TOKEN_PATH = path.join(process.cwd(), 'token.json');
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
