06608c0a4055ce1662688f4003a3cbb838e4ec94
DankLord69420 <stevecraftkid@gmail.com>
2024-04-01 21:57:09 +0100

    cors headers

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ const TOKEN_PATH = path.join(process.cwd(), 'token.json');
 
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
