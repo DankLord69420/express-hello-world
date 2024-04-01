2cb0fd03be95eeb31ab6ce683a6693744a5c1d34
DankLord69420 <stevecraftkid@gmail.com>
2024-04-01 22:01:45 +0100

    Aceitar tudo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const TOKEN_PATH = path.join(process.cwd(), 'token.json');
 
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
